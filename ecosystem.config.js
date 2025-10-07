958a7e08ccaa9992ec80df11103eaf82c0909248
surajaherrao <suraj.a@york.ie>
2025-10-07 14:14:07 +0530

    chore: Increase memory limit in PM2 configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: '2G',
       env: {
         NODE_ENV: 'production',
         PORT: 5000
@@ -29,7 +29,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: '2G',
       env: {
         NODE_ENV: 'production'
       },
